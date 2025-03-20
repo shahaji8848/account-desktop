de465ef35c839e3fc836fcee00b9d1e60fd2ac20
VishvParikh <vishvparikh@8848digital.com>
2025-03-20 12:36:33 +0530

    feat: add template api

--- src/apis/upload_bank_statement.ts
+++ src/apis/upload_bank_statement.ts
@@ -7,12 +7,11 @@ const headers = {
 export async function UploadFile(args: any) {
   try {
     let api_url = 'api/method/upload_file';
-    const currentTime = new Date();
     let params: any = new FormData();
     params.append('file', args.file);
     params.append('doctype', 'Bank Statement Import');
     params.append('fieldname', 'import_file');
-    params.append('docname', `Bank Statement Import on ${currentTime}`);
+    params.append('docname', args.docname);
 
     const response = await fetch(`${baseUrl}${api_url}`, {
       method: 'POST',
@@ -38,7 +37,8 @@ export async function GetPreviewFromTemplate(args: any) {
     let api_url = 'api/method/erpnext.accounts.doctype.bank_statement_import.bank_statement_import.get_preview_from_template';
     let params: any = {
       import_file: args.file_url,
-      data_import: args.attached_to_name,
+      data_import: args.docname,
+      google_sheets_url: args.google_sheets_url,
     };
 
     const response = await fetch(`${baseUrl}${api_url}`, {
@@ -58,5 +58,35 @@ export async function GetPreviewFromTemplate(args: any) {
   } catch (error) {
     console.error('Error in GetPreviewFromTemplate:', error);
     return { error: true, message: 'An unexpected error occurred while preview from template.' };
+  }
+}
+
+export async function DownloadTemplate(args: any) {
+  try {
+    let api_url = 'api/method/frappe.core.doctype.data_import.data_import.download_template';
+    let params: any = {
+      doctype: "Bank Transaction",
+      export_records: "5_records",
+      export_fields: {"Bank Transaction":["date","deposit","withdrawal","description","reference_number","bank_account","currency"]},
+    };
+
+    const response = await fetch(`${baseUrl}${api_url}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: args?.token,
+      },
+      body: JSON.stringify(params),
+    });
+
+    if (!response.ok) {
+      return { error: true, message: `Failed to DownloadTemplate: ${response.statusText}` };
+    }
+
+    return await response.json();
+
+  } catch (error) {
+    console.error('Error in DownloadTemplate:', error);
+    return { error: true, message: 'An unexpected error occurred while Download template.' };
   }
 }
